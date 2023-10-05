353c46c041eb79b5aea19530354918bb7687a9b5
Rayane Vieira <rayvieiraaa@gmail.com>
2023-10-05 15:39:21 -0300

    tudo mami

--- src/main/java/com/itb/brazurista/turismo/controller/UsuarioController.java
+++ src/main/java/com/itb/brazurista/turismo/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.itb.brazurista.turismo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +14,7 @@ import com.itb.brazurista.turismo.model.Usuario;
 import com.itb.brazurista.turismo.repository.UsuarioRepository;
 
 @Controller
-@RequestMapping("/brazurista/usuario")
+@RequestMapping("/brazurista")
 public class UsuarioController {
 
 	@Autowired
@@ -24,22 +27,35 @@ public class UsuarioController {
 		return "cadastro";
 	}
 	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
+	List<Usuario> listaDeUser = new ArrayList<Usuario>();
+	
+	@GetMapping("/controleradm")
+	public String contolerUser(Model model) {
+		 model.addAttribute("listaDeUser", listaDeUser);
+		 return "crud";
+	}
 	// Cadastrar o usuário
 	
 	@PostMapping("/add-usuario")
 	public String addNovoUsuario(Usuario usuario, Model model) {
-		
 		usuario.setCodStatusUsuario(true);
 		usuarioRepository.save(usuario);
+		listaDeUser.add(usuario);
 		
-		return "redirect:/brazurista/usuario/cadastro";
+		return "redirect:/brazurista/cadastro";
 	}
 	
 	// Abrir formulário de Login
 	@GetMapping("/cadastro/sucesso")
 	public String showPageSucessoCadastro() {
 		
-		return "sucesso-usuario";
+		return "index";
 	}
 	
 	// Abrir formulário de edição (perfil)
